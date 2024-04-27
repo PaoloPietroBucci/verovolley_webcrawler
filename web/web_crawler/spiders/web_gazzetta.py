9cb48d607c9bd821d883105b28fb92974bdff817
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2024-04-27 18:18:03 +0200

    Deleted nested folders and useless files

---
+++ web/web_crawler/spiders/web_gazzetta.py
@@ -0,0 +1,53 @@
+from scrapy import Spider, Request
+from scrapy.selector import Selector
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from web_crawler.items import WebCrawlerItem
+
+
+class WebCrawlerGazzetta(Spider):
+    name = "web_crawler_gazzetta"
+
+    allowed_domains = ["dal15al25.gazzetta.it"]
+    start_urls = [
+        "https://dal15al25.gazzetta.it/",
+    ]
+
+    def parse(self, response):
+        # Logic of how to extract the HTML
+        article_list = Selector(response).xpath('//*[@class="articles-list"]/article/div/div/p/a')
+        print(article_list)
+
+        next_page = Selector(response).xpath('//*[@class="next-posts"]/a/@href').extract()
+
+        next_page_link = next_page[0] if next_page else None
+
+        for article in article_list:
+            # Trigger recursively the parsing of articles
+            article_link = article.xpath('@href').extract()[0]
+            yield Request(article_link, callback=self.parse_articles)
+
+        # Recursively go to the next page
+        if next_page_link:
+            yield Request(next_page_link, callback=self.parse)
+
+
+    def parse_articles(self, response):
+        item = WebCrawlerItem()
+        item['title'] = response.xpath('//*[@class="article-title"]/h1/text()').extract()[0]
+        item['content'] = ' '.join(response.xpath('//*[@class="article-content"]/p/text()').extract())
+        item['link'] = response.url
+        item['comments'] = []
+
+        comments = response.xpath('//*[@class="commentlist"]/li')
+        for comment in comments:
+            comment_obj = {
+                'user': comment.xpath('.//*[@class="comment_author"]/text()').extract()[0],
+                'created_at_utc': comment.xpath('.//*[@class="comment_time"]/text()').extract()[0],
+                'text': " ".join(comment.xpath('.//*[@class="comment_text"]/p/text()').extract()),
+            }
+            item['comments'].append(comment_obj)
+
+
+        yield item
---
+++ web/web_crawler/spiders/web_ivolleymagazine.py
@@ -0,0 +1,27 @@
+from scrapy import Spider, Request
+import re
+from ..items  import BlogPostItem
+
+
+class OASportSpider(Spider):
+    name = 'ivolleymagazine'
+    allowed_domains = ['ivolleymagazine.it']
+    start_urls = ['https://www.ivolleymagazine.it/category/news/']
+
+    def parse(self, response):
+        pages_article = response.xpath('//h2[contains(@class, "entry-title")]/a/@href').getall()
+        for article in pages_article:
+            link = response.urljoin(article)
+            yield Request(url=link, callback=self.parse_article)
+        next_page = response.xpath('//li[contains(@class, "next")]/a/@href').get()
+        yield Request(url=next_page, callback=self.parse)
+        
+    def parse_article(self, response):
+        post = BlogPostItem()
+        article_paragrafs = response.xpath('//div[contains(@class, "entry-content")]/p/text()').getall()
+        unique_text = ' '.join(article_paragrafs)
+        article_title = response.xpath('//h1/text()').get()
+        post['title'] = article_title.strip()
+        post['content'] = unique_text.strip()
+        yield post
+#  scrapy crawl ivolleymagazine -o output.json
